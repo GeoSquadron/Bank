b3776cd6aa8babe8718642217483e682354ff0ae
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-07-28 16:53:46 -0300

    first

---
+++ Bank/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class
+    /// </summary>
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+
+        private BankAccount()
+        { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+    }
+}
