8b2cfbe3fd5c5ae038d6a2d753d19a8e18e484da
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-07-29 22:43:51 -0300

    NOvo método paracriação de testes.

--- BankTest/BankAccountTests.cs
+++ BankTest/BankAccountTests.cs
@@ -28,5 +28,37 @@ namespace BankTest
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+
+        // unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //act
+            account.Debit(debitAmount);
+
+            //assert is handled by ExpectedException
+        }
+
+        // unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //act
+            account.Debit(debitAmount);
+
+            //assert is handled by ExpectedException
+        }
     }
 }
